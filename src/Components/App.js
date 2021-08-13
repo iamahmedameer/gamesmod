cb3836cd7d53c8b0b03c109b1a30e5e232d9497c
Muhammad Ahmed <iamahmedameer@gmail.com>
2021-08-13 06:22:18 +0100

    added disclaimer

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,11 +2,27 @@ import React from "react";
 import Menu from "./Menu/Menu";
 import Navbar from "./Navbar/Navbar";
 
+const Disclaimer = () => {
+  return (
+    <div
+      style={{
+        width: "100%",
+        fontSize: 12,
+        color: "white",
+        textAlign: "end",
+      }}
+    >
+      Not mobile responsive
+    </div>
+  );
+};
+
 const App = () => {
   return (
     <div>
       <Menu />
       <Navbar />
+      <Disclaimer />
     </div>
   );
 };
